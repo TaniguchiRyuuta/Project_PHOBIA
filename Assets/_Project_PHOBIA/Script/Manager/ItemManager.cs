45bcce386d66d97bb91bfb97ba1605fcbcccb165
MurataNaoya <naoya.murata1@gmail.com>
2026-09-18 16:50:34 +0900

    PlayerControllerの回転方法の変更　ItemManagerにinitialItemとSetInitialItem()を追加　プレイヤーの手のマテリアルを付与

--- Assets/_Project_PHOBIA/Script/Manager/ItemManager.cs
+++ Assets/_Project_PHOBIA/Script/Manager/ItemManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -11,11 +12,14 @@ namespace PJ_PHOBIA
         public int CurrentItemID { get; set; } = 0;
         private int previousID;
 
+        [SerializeField] private ItemData initialItem;
         private void Awake()
         {
             itemList = new List<GameObject>();
-            itemList.Add(Items[(int)ItemData.Hand]);
-            itemList.Add(Items[(int)ItemData.FlashLight]);
+            itemList.Add(Items[(int)ItemData.Hand]);        //この初期化のやり方は怪しい。変更の余地あり。
+            itemList.Add(Items[(int)ItemData.FlashLight]);　//同上
+
+            SetInitialItem((int)initialItem);
         }
         public void AddItem(int ID)
         {
@@ -44,6 +48,7 @@ namespace PJ_PHOBIA
         }
         public void DeactivateItem(int target)
         {
+            Debug.Log("[ItemManager] DeativateItem");
             itemList[target].SetActive(false);
         }
         public void UseItem(int target)
@@ -51,6 +56,20 @@ namespace PJ_PHOBIA
             itemList[target].GetComponent<EventBehaviour>()?.OnEvent();
         }
 
+        private void SetInitialItem(int ID)
+        {
+            Debug.Log($"[ItemManager] SetInitialItem {Enum.GetNames(typeof(ItemData))[CurrentItemID]}");
+            if (!itemList.Contains(Items[ID]))
+            {
+                Debug.LogWarning($"[ItemManager] {Enum.GetNames(typeof(ItemData))[ID]}は所持していないアイテムです");
+                return;
+            }
+
+            DeactivateItem(CurrentItemID);
+
+            ActivateItem(ID);
+            CurrentItemID = ID;
+        }
         public void SwitchItem(int input)
         {
             previousID = CurrentItemID;
